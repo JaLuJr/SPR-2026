98b0f7a8b7af436cf632da7be51ace10d18ee271
JaLuJr <jarianlauderdale24@gmail.com>
2026-04-15 21:56:51 -0500

    Total Formatting Test

---
+++ modes.js
@@ -0,0 +1,23 @@
+function toggleContrast() {
+  document.body.classList.toggle("high-contrast");
+}
+
+function toggleDyslexia() {
+  document.body.classList.toggle("dyslexia");
+}
+
+function toggleLargeText() {
+  document.body.classList.toggle("large-text");
+}
+
+function toggleSoftMode() {
+  document.body.classList.toggle("soft-mode");
+}
+
+function toggleBlueYellow() {
+  document.body.classList.toggle("blue-yellow");
+}
+
+function toggleRedBlack() {
+  document.body.classList.toggle("red-black");
+}
